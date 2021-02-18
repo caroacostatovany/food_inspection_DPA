e1940d7e2adb0c6a3aa037ab328eb5dc4dfe807e
caroacostatovany <carolina.acostatovany@gmail.com>
2021-02-18 15:49:20 -0600

    Agregar archivo general.py con get_s3_credentials y read_yaml_file

---
+++ src/utils/general.py
@@ -0,0 +1,45 @@
+"""
+Módulo de funciones generales
+"""
+import yaml
+
+
+def read_yaml_file(yaml_file):
+    """
+    Carga las Configuración de un .yaml
+    ==========
+    * Args:
+         - yaml_file
+    * Return:
+         - config: configuraciones del yaml
+    ==========
+    Ejemplo:
+        >> credentials = read_yaml_file(credentials_file)
+    """
+
+    config = None
+    try:
+        with open(yaml_file, 'r') as f:
+            config = yaml.safe_load(f)
+    except:
+        raise FileNotFoundError('No se pudo leer el archivo')
+
+    return config
+
+
+def get_s3_credentials(credentials_file):
+    """
+    Se obtienen las credenciales para acceder a un s3
+    ==========
+    * Args:
+         - credentials_file
+    * Return:
+         - s3_creds: Las credenciales existentes en la configuración s3
+    ==========
+    Ejemplo:
+        >> s3_creds = get_s3_credentials("./conf/local/credentials.yaml")
+    """
+    credentials = read_yaml_file(credentials_file)
+    s3_creds = credentials['s3']
+
+    return s3_creds
